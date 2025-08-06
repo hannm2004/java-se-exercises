66549ed42d8dcd40861fea6b5f3b1f7c34ec7786
hannm2004 <hanminh2709@gmail.com>
2025-08-06 23:08:38 +0700

    Assignment_101

---
+++ Assignment_101/ArithmeticExercise3.java
@@ -0,0 +1,9 @@
+package fa.training.assignment1;
+
+public class ArithmeticExercise3 {
+
+  public static void main(String[] args) {
+    double result = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+    System.out.println(result);
+  }
+}
